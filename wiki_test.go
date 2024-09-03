2323edd6b7c09058fe0c66be2ba0eb7269d79aba
humanspider <mswolbaum@gmail.com>
2024-09-03 18:11:04 -0500

    Add redirect tests

--- wiki_test.go
+++ wiki_test.go
@@ -52,4 +52,42 @@ func TestServer(t *testing.T) {
 			t.Errorf("Expected status code 200. Actual %d\n", resp.StatusCode)
 		}
 	})
+
+	t.Run("Redirects on wiki page does not exist", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, ts.URL+"/view/NewPage", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected status code 200. Actual %d\n", resp.StatusCode)
+		}
+
+		if resp.Request.URL.Path != "/edit/NewPage" {
+			t.Errorf("Expected redirect to /edit/NewPage. Actual %s", resp.Request.URL.Path)
+		}
+	})
+}
+
+func TestHandler(t *testing.T) {
+	t.Run("Redirects on wiki page does not exist", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "http://example.com/view/NewPage", nil)
+		w := httptest.NewRecorder()
+		viewHandler(w, req)
+
+		resp := w.Result()
+
+		if resp.StatusCode != 302 {
+			t.Errorf("Expected status code 302. Actual %d.", resp.StatusCode)
+		}
+		if resp.Header.Get("Location") != "/edit/NewPage" {
+			t.Errorf("Expected /edit/NewPage. Actual %s.", resp.Header.Get("Location"))
+		}
+	})
 }
